049bf80502ce77f13411cb309f5937fe02eae8c4
Demi-la <adebiyio28@gmail.com>
2023-04-11 16:51:48 +0100

    add the functionality and responsiveness

--- src/Home/Price.js
+++ src/Home/Price.js
@@ -1,32 +1,137 @@
 import React from 'react'
+import { useState } from 'react';
 import './Price.css'
 
+// const PricingValues= {
+//   1: {
+//     pageviews: "10K",
+//     monthly: 8,
+//     yearly: 72,
+//   },
+//   2: {
+//     pageviews: "50k",
+//     monthly: 12,
+//     yearly: 108,
+//   },
+//   3: {
+//     pageviews: "100K",
+//     monthly: 16,
+//     yearly: 144,
+//   },
+//   4: {
+//     pageviews: "500k",
+//     monthly: 24,
+//     yearly: 216,
+//   },
+//   5: {
+//     pageviews: "1M",
+//     monthly: 36,
+//     yearly: 324,
+//   },
+// };
 
-function price() {
+
+
+
+function Price() {
+  // const [isYearlyBilling, setIsYearlyBilling] = useState(false);
+  // const [pageviews, setPageviews] = useState(100000);
+
+  // const handleBillingToggle = () => {
+  //   setIsYearlyBilling(!isYearlyBilling);
+  // };
+
+  // const handleSliderChange = (event) => {
+  //   setPageviews(parseInt(event.target.value));
+  // };
+
+  const [pageviews, setPageviews] = useState(100);
+  const [price, setPrice] = useState(8);
+  const [discount, setDiscount] = useState(false);
+
+
+  const handleSliderChange = (event) => {
+    const value = event.target.value;
+
+    let pageviews = 0;
+    let price = 0;
+
+    switch (value) {
+      case "1":
+        pageviews = "10K";
+        price = 8;
+        break;
+      case "2":
+        pageviews = "50K";
+        price = 12;
+        break;
+      case "3":
+        pageviews = "100K";
+        price = 16;
+        break;
+      case "4":
+        pageviews = "500K";
+        price = 24;
+        break;
+      case "5":
+        pageviews = "1M";
+        price = 36;
+        break;
+      default:
+        break;
+    }
+
+    setPageviews(pageviews);
+    setPrice(price);
+    
+
+
+    if (discount) {
+      price *= 0.75;
+    }
+    
+
+ 
+  };
+  const handleToggleChange = () => {
+    setDiscount(!discount)};
+  
+
   return (
     <div className='price-container'>
-      
+     
       <div className='price-parent'>
+      
         <h1>Simple, trafic-based pricing</h1>
         <p>sign-up for our 30-days trial. No credit card required</p>
       </div>
+    
       <div className='price-box'>
           <div className='first-text'>
-            <h3 className='pageviews page-views'>100K PAGEVIEWS</h3>
-            <p className='pageviewst'><span className='price price-change'>$16.00</span>/month</p>
+            <h3 className='pageviews page-views'>{pageviews} PAGEVIEWS</h3>
+            <p className='pageviews page'><span className='price price-change'> {discount ? `$${(price * 0.75).toFixed(2)}` : `$${price.toFixed(2)}`}
+          </span>  <span id='month'>{discount ? "/year" : "/month"}</span></p>
           </div>
           <div className='range-container'>
 
           <div className='range'>
-             <input type="range" min="0" max="100" value="50" step="25" aria-label="Slider input"/>
+             <input type="range" min="1" max="5" defaultValue="1" step="1" aria-label="Slider input"  onChange={handleSliderChange}/>
+             
           </div>
+          <p className='pageviews page'><span className='price price-change'> {discount ? `$${(price * 0.75).toFixed(2)}` : `$${price.toFixed(2)}`}
+          </span>  <span id='month'>{discount ? "/year" : "/month"}</span></p>
           </div>
        
       <div className='price-plan'>
-        <p>Monthly Billing</p>
-        <label className='switch'>
-           <input type="checkbox"/>
+        <p id='price__text'>Monthly Billing</p>
+        <label  htmlFor="billing-toggle" className='switch'>
+           <input type="checkbox"    
+            id="billing-toggle"  
+            onChange={handleToggleChange} 
+           />
            <span className='slider round'></span>
+         
+
         </label>
         <p>Yearly Billing </p>
         <span className='discount'>25% Discount</span>
@@ -43,8 +148,9 @@ function price() {
        </div>
       </div>
       </div>
+      <h2 className='attribute'>Project from <a href='https://www.frontendmentor.io'>frontend mentor</a> coded by Adebiyi Oluwaseun</h2>
     </div>
   )
 }
 
-export default price
+export default Price
